000d84f647fb279600c564d35a4cf3976b2e1dd5
hzm <467251648@qq.com>
2023-05-24 11:23:21 +0800

    feat: 尝试handleValue传值

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,10 @@
 // [name].css 只支持在全局将应用于你的应用程序中的所有页面和组件。 由于样式表的全局特性，并且为了避免冲突，你应该 只在 pages/_app.js 文件中导入（import）样式表。
 // import "./index.css";
 
+import { useState, useRef } from "react";
+
 //  通过 [name].module.css 文件命名约定来支持 CSS 模块 。
 import styles from "./about.module.css";
-
-import { useState } from "react";
 
 // 在 export default 前处理
 export async function getStaticProps(props) {
@@ -17,23 +17,30 @@ export async function getStaticProps(props) {
 	};
 }
 
-export default function aboutPage(that) {
+export default function AboutPage(that) {
 	let [value, setValue] = useState(12);
 
-	console.log("上下文", that);
+	// console.log("上下文", that);
 
-	function handleValue(e) {
-		console.log("点击了", e);
-		setValue(value + 1);
+	function handleValue(type = "add", e) {
+		console.log("e", type, e);
+		if (type === "add") {
+			setValue(value + 1);
+		} else {
+			setValue(value - 1);
+		}
 	}
 
 	return (
 		<div className={styles.ab}>
 			<div className={styles.ab__title}>num value: {value}</div>
-			<div className={styles.ab__btn} onClick={handleValue}>
+			<div className={styles.ab__btn} onClick={() => handleValue("add", event)}>
 				+
 			</div>
-			<div className={styles.ab__btn} onClick={handleValue}>
+			<div
+				className={styles.ab__btn}
+				onClick={() => handleValue("reduce", event)}
+			>
 				-
 			</div>
 		</div>
